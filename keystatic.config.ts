9606b376009ca439d7270b3bceed7b8df7b6e7cd
Milen <neo-art@users.noreply.github.com>
2024-04-26 00:44:19 +0200

    Update keystatic.config.ts

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,3 +1,4 @@
+import { collections } from '@/content/config';
 import { config, fields, collection } from '@keystatic/core';
 
 export default config({
@@ -27,24 +28,26 @@ export default config({
   },
 });
 
-// collections: {
-//     pages: collection({
-//       label: 'Pages',
-//       slugField: 'title',
-//       path: 'src/content/pages/*',
-//       format: { contentField: 'content' },
-//       schema: {
-//         title: fields.slug({ name: { label: 'Title' } }),
-//         date: fields.date({ label: 'Date' }),
-//         description: fields.text({ label: 'Description' }),
-//         content: fields.document({
-//           label: 'Content',
-//           formatting: true,
-//           dividers: true,
-//           links: true,
-//           images: true,
-//         }),
-//       },
-//     }),
-//   },
-// });
+collections: {
+pages: collection({
+  id: String,
+  title: String,
+  meta_title: { type: String, optional: true },
+  description: { type: String, optional: true },
+  image: { type: String, optional: true },
+  layout: { type: String, optional: true },
+  slugTitle: {
+    name: {
+      label: String
+    }
+  },
+  content: {
+    label: 'Content',
+    formatting: true,
+    dividers: true,
+    links: true,
+    images: true
+  },
+});
+}
+
