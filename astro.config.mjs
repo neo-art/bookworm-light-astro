12057ad5c217af8635aa108bc9913960439e3ee9
Milen <neo-art@users.noreply.github.com>
2024-04-26 00:11:18 +0200

    Config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,50 +7,34 @@ import { defineConfig, squooshImageService } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
+import keystatic from '@keystatic/astro';
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
+  output: 'hybrid',
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
-  integrations: [
-    react(),
-    sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    AutoImport({
-      imports: [
-        "@/shortcodes/Button",
-        "@/shortcodes/Accordion",
-        "@/shortcodes/Notice",
-        "@/shortcodes/Video",
-        "@/shortcodes/Youtube",
-        "@/shortcodes/Tabs",
-        "@/shortcodes/Tab",
-      ],
-    }),
-    mdx(),
-  ],
+  integrations: [react(), keystatic(), markdoc(), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), AutoImport({
+    imports: ["@/shortcodes/Button", "@/shortcodes/Accordion", "@/shortcodes/Notice", "@/shortcodes/Video", "@/shortcodes/Youtube", "@/shortcodes/Tabs", "@/shortcodes/Tab"]
+  }), mdx(), markdoc()],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }]],
     shikiConfig: {
       theme: "one-dark-pro",
-      wrap: true,
+      wrap: true
     },
-    extendDefaultPlugins: true,
-  },
+    extendDefaultPlugins: true
+  }
 });
---
+++ keystatic.config.ts
@@ -0,0 +1,50 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        date: fields.date({ label: 'Date' }),
+        description: fields.text({ label: 'Description' }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
+
+// collections: {
+//     pages: collection({
+//       label: 'Pages',
+//       slugField: 'title',
+//       path: 'src/content/pages/*',
+//       format: { contentField: 'content' },
+//       schema: {
+//         title: fields.slug({ name: { label: 'Title' } }),
+//         date: fields.date({ label: 'Date' }),
+//         description: fields.text({ label: 'Description' }),
+//         content: fields.document({
+//           label: 'Content',
+//           formatting: true,
+//           dividers: true,
+//           links: true,
+//           images: true,
+//         }),
+//       },
+//     }),
+//   },
+// });
